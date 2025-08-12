23e9edf53094f70b706aa2cc5db80d099d96d8e0
Roman <romanhassan328@gmail.com>
2025-08-13 02:47:35 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,17 @@
+/**
+ *************************************************
+ *            MATH NEEDED PROGRAMMING            *
+ ************************************************* 
+ * 1. Number: Positive, Negative, 0 [50, -40, 0]
+ * 2. Integer: 2, 88, [Odd, Even]: 2, 4, 457
+ * 3. Float: 5.8. 2.3
+ * 4. Operation: +, -, *, /
+ * 5. More Operation: %, [ ]
+ * 6. Percentage: 5%, 50 taka er 10% koto?
+ * 7. Sum: 14, 16, 18
+ * 8. Average: 12, 45, 25
+ * 9. Max or Min: 12, 18, 16
+ * 10. Series: 
+ * 11. Area: Triangle 
+ * 12. 
+ */
---
+++ variable.js
@@ -0,0 +1,31 @@
+/**
+ * Variable ==> Vary + able
+ *  5 things to declare a variable
+ */
+var weight = 21 ;
+var price = 100 ;
+var year = 2004 ;
+var money = 2000 ;
+money = 200 ;
+
+var age ;
+console.log(money);
+console.log(weight);
+
+
+
+
+
+
+var age = 70 ;
+age=25;
+console.log(age);
+
+
+
+
+
+
+
+
+
