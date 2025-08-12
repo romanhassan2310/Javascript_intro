e392566f4f46e8ff358dadb948a9996473d0b954
Roman <romanhassan328@gmail.com>
2025-08-13 03:12:19 +0600

    added types.js

---
+++ types.js
@@ -0,0 +1,19 @@
+// string.
+var name = "Roman" ;
+
+var address = "Jamalpur Sadar Ramnagor";
+
+var country = "Bangladesh";
+
+// Boolean.
+
+var isRich = true;
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+console.log( typeof country);
+
+// Number.
+var price = 500;
+console.log(typeof price);
