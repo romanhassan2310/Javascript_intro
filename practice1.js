59738cb7c0718bd7438afbaebff690929d42a50e
Roman <romanhassan328@gmail.com>
2025-08-14 15:43:04 +0600

    added practice1

---
+++ practice1.js
@@ -0,0 +1,8 @@
+var totalTaka = 1000;
+var orange = 400;
+var apple =300;
+var totalCost = orange + apple;
+// var orangeAndApple =700;
+// var returnTaka = 1000 - 700;
+
+console.log(totalTaka - totalCost);
