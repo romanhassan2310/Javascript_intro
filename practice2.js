d3a0a3349c3ef81ee49fc6c4dabf26468a1e1ffc
Roman <romanhassan328@gmail.com>
2025-08-14 16:15:57 +0600

    added practice

---
+++ practice2.js
@@ -0,0 +1,9 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totolSubject = 5;
+var totalNumber = mathematics + biology + chemistry + physics + bangla;
+var average = totalNumber / totolSubject;
+console.log(average.toFixed(2));
---
+++ practice3.js
@@ -0,0 +1,5 @@
+var totalNumber =119;
+var divideNumber = 5;
+
+var remainder= 119 % 5;
+console.log(remainder);
